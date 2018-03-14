db04d48ea58c1e13e26cf9a8062e661c9d1a3ad2
Bastian Fuchshofer <bastian.f.1993@web.de>
2018-03-14 19:03:34 +0100

    first commit

---
+++ app/src/main/java/com/example/basti/findaroom_kotlin/VerificationActivity.kt
@@ -0,0 +1,18 @@
+package com.example.basti.findaroom_kotlin
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by Basti on 14.03.2018.
+ */
+
+class VerificationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_verification)
+
+    }
+
+}
